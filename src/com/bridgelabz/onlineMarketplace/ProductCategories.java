20b5bde6a86fb3d7d39bb3298e5098fbcad47211
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-27 14:56:02 +0530

    [Sanya] Add: Categories available of product

---
+++ src/com/bridgelabz/onlineMarketplace/ProductCategories.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.onlineMarketplace;
+
+// Enum for Book Categories
+enum BookCategory {
+    FICTION, NON_FICTION, EDUCATIONAL
+}
+
+// Enum for Clothing Categories
+enum ClothingCategory {
+    MEN, WOMEN, KIDS
+}
+
+// Enum for Gadget Categories
+enum GadgetCategory {
+    MOBILE, LAPTOP, ACCESSORY
+}
