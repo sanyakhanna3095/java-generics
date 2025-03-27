14e147fea30c9d4ff70ca7060c58d3876d7ec276
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-27 19:42:49 +0530

    [Sanya] Add: Abstract base class for course types and Concrete course types

--- src/com/bridgelabz/courseManagementSystem/CourseType.java
+++ src/com/bridgelabz/courseManagementSystem/CourseType.java
@@ -13,3 +13,49 @@ public abstract class CourseType {
 }
 
 
+class ExamCourse extends CourseType {
+    private int examWeightage;
+
+    public ExamCourse(String courseName, String instructor, int examWeightage) {
+        super(courseName, instructor);
+        this.examWeightage = examWeightage;
+    }
+
+    @Override
+    public void displayCourseDetails() {
+        System.out.println("Exam-Based Course: " + courseName + " | Instructor: " + instructor + " | Exam Weightage: " + examWeightage + "%");
+    }
+}
+
+
+class AssignmentCourse extends CourseType {
+    private int totalAssignments;
+
+    public AssignmentCourse(String courseName, String instructor, int totalAssignments) {
+        super(courseName, instructor);
+        this.totalAssignments = totalAssignments;
+    }
+
+    @Override
+    public void displayCourseDetails() {
+        System.out.println("Assignment-Based Course: " + courseName + " | Instructor: " + instructor + " | Total Assignments: " + totalAssignments);
+    }
+}
+
+
+class ResearchCourse extends CourseType {
+    private String researchTopic;
+
+    public ResearchCourse(String courseName, String instructor, String researchTopic) {
+        super(courseName, instructor);
+        this.researchTopic = researchTopic;
+    }
+
+    @Override
+    public void displayCourseDetails() {
+        System.out.println("Research-Based Course: " + courseName + " | Instructor: " + instructor + " | Research Topic: " + researchTopic);
+    }
+}
+
+
+
