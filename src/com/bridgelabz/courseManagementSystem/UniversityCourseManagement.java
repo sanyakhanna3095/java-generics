bfc7398ede9cc39c7f209976a792c29a92a198f5
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-27 20:16:57 +0530

    [Sanya] Add: Main class for implentation

---
+++ src/com/bridgelabz/courseManagementSystem/UniversityCourseManagement.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.courseManagementSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniversityCourseManagement {
+    public static void main(String[] args) {
+        // Creating different courses
+        ExamCourse mathExam = new ExamCourse("Mathematics", "Dr. Smith", 70);
+        AssignmentCourse historyAssignment = new AssignmentCourse("History", "Prof. Johnson", 5);
+        ResearchCourse physicsResearch = new ResearchCourse("Physics", "Dr. Brown", "Quantum Mechanics");
+
+        // Creating generic course objects
+        Course<ExamCourse> course1 = new Course<>(mathExam);
+        Course<AssignmentCourse> course2 = new Course<>(historyAssignment);
+        Course<ResearchCourse> course3 = new Course<>(physicsResearch);
+
+        // Creating Course Catalogs
+        CourseCatalog<ExamCourse> examCatalog = new CourseCatalog<>();
+        CourseCatalog<AssignmentCourse> assignmentCatalog = new CourseCatalog<>();
+        CourseCatalog<ResearchCourse> researchCatalog = new CourseCatalog<>();
+
+        // Adding courses to catalogs
+        examCatalog.addCourse(course1);
+        assignmentCatalog.addCourse(course2);
+        researchCatalog.addCourse(course3);
+
+        // Display catalogs
+        examCatalog.displayAllCourses();
+        assignmentCatalog.displayAllCourses();
+        researchCatalog.displayAllCourses();
+
+        // Using wildcard method to display all courses
+        List<CourseType> allCourses = new ArrayList<>();
+        allCourses.add(mathExam);
+        allCourses.add(historyAssignment);
+        allCourses.add(physicsResearch);
+
+        CourseCatalog.displayCourses(allCourses);
+    }
+}
+
+
+
