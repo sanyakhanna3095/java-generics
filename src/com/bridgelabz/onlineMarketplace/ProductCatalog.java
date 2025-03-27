466f6fdf74b5528e0ec8ec31b5f7c8e4c7cdc758
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-27 15:42:47 +0530

    [Sanya] Add: multiple product categories

--- src/com/bridgelabz/onlineMarketplace/ProductCatalog.java
+++ src/com/bridgelabz/onlineMarketplace/ProductCatalog.java
@@ -3,20 +3,17 @@ package com.bridgelabz.onlineMarketplace;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductCatalog<T> {
+public class ProductCatalog<T extends Enum<T>> {
     private List<Product<T>> products = new ArrayList<>();
 
-    // Method to add a product
     public void addProduct(Product<T> product) {
         products.add(product);
     }
 
-    // Method to display all products
     public void displayCatalog() {
-        System.out.println("\nProduct Catalog:");
-        for(int i = 0; i < products.size(); i++) {
+        System.out.println("\n--- Product Catalog ---");
+        for (int i = 0; i < products.size(); i++) {
             products.get(i).displayProduct();
         }
     }
 }
-
