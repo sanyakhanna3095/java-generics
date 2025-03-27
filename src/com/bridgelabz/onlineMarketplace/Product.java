d51e048de0655adaf8e1714bd999655d1be3f57b
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-27 15:41:39 +0530

    [Sanya] Add: multiple product categories in the same catalog

--- src/com/bridgelabz/onlineMarketplace/Product.java
+++ src/com/bridgelabz/onlineMarketplace/Product.java
@@ -1,19 +1,17 @@
 package com.bridgelabz.onlineMarketplace;
 
 // Generic Product class restricted to specific categories
-public class Product<T> {
+public class Product<T extends Enum<T>> {
     private String name;
     private double price;
     private T category;
 
-    // Constructor
     public Product(String name, double price, T category) {
         this.name = name;
         this.price = price;
         this.category = category;
     }
 
-    // Getters for name and price
     public String getName() {
         return name;
     }
@@ -22,25 +20,16 @@ public class Product<T> {
         return price;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public T getCategory() {
         return category;
     }
 
-    // Method to apply discount
-    public void applyDiscount(double percentage) {
-        if (percentage > 0 && percentage <= 100) {
-            this.price -= (this.price * percentage / 100);
-            System.out.println("Discount applied! New Price of " + name + ": Rs." + price);
-        }
-        else {
-            System.out.println("Invalid discount!");
-        }
-    }
-
-
-    // Display product details
     public void displayProduct() {
-        System.out.println("Product: " + name + " || Category: " + category + " || Price: Rs." + price);
+        System.out.println("Product: " + name + " | Price: $" + price + " | Category: " + category);
     }
 }
 
